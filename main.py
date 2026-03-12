4495a36fbe902a7ed57749c1bd53e1fdcc4149b2
Artur Bertin <arturbertin2006@gmail.com>
2026-03-11 23:31:01 -0300

    Solving

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players = json.load(file)
+        for nickname, data in players.items():
+            
+            
+
+
 
 
 if __name__ == "__main__":
